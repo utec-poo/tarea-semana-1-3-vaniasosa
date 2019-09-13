a6018fe6f9457504c18d46a297a5b1b15e6facea
vaniasosa <40771103+vaniasosa@users.noreply.github.com>
2019-09-13 16:49:18 -0500

    Update main.cpp

--- ejercicio2/main.cpp
+++ ejercicio2/main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include "Tipos.h"
 using namespace std;
-
 int main() {
-
-
-// ToDO
-
-  return 0;
+int zona, x;char claro;float y;
+cout<< "¿usted es cliente claro ? ";
+cout<< " s = si o n= no  ";
+cin>> claro;
+do{
+cout<< "ingrese numero de zona: ";
+cin>> zona;
+} while (zona > 4); 
+if (zona == 1){
+  x=212;
+  cout<< " Zona Super Vip \n ";}
+   else {
+     if (zona == 2) {
+      x= 170;
+      cout<<" Zona Vip \n"; }
+      else {
+         if (zona == 3) {
+          x= 136;
+          cout<<"Zona Preferencial \n ";}
+          else {
+            x=59;
+           cout<<"Zona General \n   ";
+          }
+     }
+  }   
+if (claro == 's')
+{  y = x* 0.20 ;
+   y = x - y; 
+   cout<< "precio a pagar es  "<< y;
+}
+else 
+{
+  cout<< "precio a pagar es  "<< x;
+}
 }
