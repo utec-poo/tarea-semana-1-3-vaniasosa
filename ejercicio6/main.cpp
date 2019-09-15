640f606b800bb72769f39fe43707c4ee43e29564
vaniasosa <40771103+vaniasosa@users.noreply.github.com>
2019-09-14 21:30:50 -0500

    Update main.cpp

--- ejercicio6/main.cpp
+++ ejercicio6/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include "Tipos.h"
 using namespace std;
-
 int main() {
-
-entero numero;
-
-
+int numero,x;
+do{
 cout << "ingrese un numero:";
-
 cin >> numero;
-
-// ToDO
-// implemente el algoritmo.
-  return 0;
-}
+} while ((numero<100) or (numero>1000)); 
+for (int i=0; x!= 0; i++)
+ {
+  x= numero/1;
+   if (x%2==0)
+   {cout<< "La ultima cifra es par \n";}
+   else 
+    {cout<< "La ultima cifra es impar \n";}
+  x= numero/10;
+  if (x%2==0)
+   {cout<< "La segunda cifra es par \n";}
+   else 
+    {cout<< "La segunda cifra es impar \n";}
+  x= numero/100;
+  if (x%2==0)
+   {cout<< "La primera cifra es par \n";}
+   else 
+    {cout<< "La primera cifra es impar \n";}
+  x= numero/1000;  
+ }
+} 
